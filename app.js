62f43e9f43ad76b1968026ea3b889d2b47740a38
Martha Sharpe <marthasharpe2020@gmail.com>
2019-10-24 16:18:51 -0400

    Handle CORS errors

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow_Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
 //Reoutes to handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
