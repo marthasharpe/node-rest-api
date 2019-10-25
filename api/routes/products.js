6d619fb3a75fb8cf1fb16c601f28239a9a70325a
Martha Sharpe <marthasharpe2020@gmail.com>
2019-10-24 20:56:59 -0400

    Handle general GET request

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,16 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    })
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
 });
 
 router.post('/', (req, res, next) => {
@@ -40,7 +47,11 @@ router.get('/:productID', (req, res, next) => {
         .exec()
         .then(doc => {
             console.log(doc);
-            res.status(200).json(doc);
+            if (doc) {
+                res.status(200).json(doc);    
+            } else {
+                res.status(404).json({message: 'No valid entry found for provided ID.'});
+            }
         })
         .catch(err => {
             console.log(err);
